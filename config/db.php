18c132599a4540bde82f9df5520cd991969f464d
pinkslot <pinkslot@github.com>
2016-12-28 06:43:02 +1000

    add config/db to .gitignore

--- config/db.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-return [
-    'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;dbname=newyear',
-    'username' => 'root',
-    'password' => '1256',
-    'charset' => 'utf8',
-];
