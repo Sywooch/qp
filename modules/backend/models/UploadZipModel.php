c73c8ea37af414154030daf9a824afa5af70861b
pinkslot <pinkslot@github.com>
2017-01-16 18:21:31 +1000

    refactor backend/UploadZipModel/catalogHandler

--- modules/backend/models/UploadZipModel.php
+++ modules/backend/models/UploadZipModel.php
@@ -22,57 +22,58 @@ class UploadZipModel extends Model
         ];
     }
 
+    public static function str2bool($str) {
+        return (string)$str === 'true';
+    }
+
     public function recursivelyAddCategory($xml, $menu) {
         if ($xml->Группы->Группа) {
             foreach ($xml->Группы->Группа as $xmlc) {
                 $c1idc = (string) $xmlc->Ид;
                 $menuc = Menu::findByC1id($c1idc);
 
-                $is_deletec = (string) $xmlc->ПометкаУдаления === 'true' ? true : false;
-                if ($is_deletec) {
-                    if (!$menuc) continue;
+                if (static::str2bool($xmlc->ПометкаУдаления) && $menuc) {
                     if ($menuc->deleteWithChildren()) {
-                        Yii::$app->session->addFlash('warning', "Категория <i>$menuc->name</i> и все вложенные в неё удалены");
+                        Yii::$app->session->addFlash('warning',
+                            "Категория <i>$menuc->name</i> и все вложенные в неё удалены");
                     }
                     else {
-                        Yii::$app->error("Возникла ошибка при удалении категории <i>$menuc->name</i>");
+                        Yii::$app->session->addFlash('error',
+                            "Возникла ошибка при удалении категории <i>$menuc->name</i>");
                     }
-                    return;
                 }
+                else {
+                    $was_new = false;
+                    if (!$menuc) {
+                        $was_new = true;
+                        $menuc = new Menu([
+                            'name' => (string)$xmlc->Наименование,
+                            'c1id' => $c1idc,
+                        ]);
+                    };
 
-                $was_new = false;
-                if (!$menuc) {
-                    $was_new = true;
-                    $menuc = new Menu([
-                        'name' => (string)$xmlc->Наименование,
-                        'c1id' => $c1idc,
-                    ]);
-                };
-
-                if (!$menuc->parents(1)->one() || ($menuc->parents(1)->one()->id != $menu->id)) {
-                    if (!$menuc->appendTo($menu)) {
-                        Yii::$app->error("Возникла ошибка при добавлении категории <i>$menuc->name</i> ");
-                    } else {
-                        if ($was_new) {
-                            Yii::$app->session->addFlash('success', "Категория <i>$menuc->name </i> добавлена");
+                    if (!$menuc->parents(1)->one() || ($menuc->parents(1)->one()->id != $menu->id)) {
+                        if ($menuc->validate() && $menuc->appendTo($menu)) {
+                            if ($was_new) {
+                                Yii::$app->session->addFlash('success', "Категория <i>$menuc->name </i> добавлена");
+                            }
+                            else {
+                                Yii::$app->session->addFlash('warning', "Категория <i>$menuc->name </i> перенесена");
+                            }
+                            $this->recursivelyAddCategory($xmlc, $menuc);
                         }
                         else {
-                            Yii::$app->session->addFlash('warning', "Категория <i>$menuc->name </i> перенесена");
+                            Yii::$app->session->addFlash('error',
+                                "Возникла ошибка при добавлении категории <i>$menuc->name</i>. " .
+                                implode(', ', $menuc->getFirstErrors()));
                         }
                     }
                 }
-                $this->recursivelyAddCategory($xmlc, $menuc);
             }
         }
     }
 
-    public function catalogHandler($xml_data) {
-        $xml = new SimpleXMLElement($xml_data);
-        $root = Menu::getRoot();
-
-
-//        var_dump($xml->Классификатор->Группы->Группа->Группы->Группа[0]->Группы->Группа[0]->Группы->Группа);
-//        exit;
+    public function catalogHandler($xml) {
         $this->recursivelyAddCategory($xml->Классификатор->Группы->Группа, Menu::getRoot());
     }
 
