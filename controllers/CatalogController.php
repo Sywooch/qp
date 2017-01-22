170ce8d02bff7201fe86cf3d62057b325ca82c21
pinkslot <pinkslot@github.com>
2017-01-23 05:01:49 +1000

    add product view

--- controllers/CatalogController.php
+++ controllers/CatalogController.php
@@ -7,17 +7,13 @@ use yii\web\NotFoundHttpException;
 
 class CatalogController extends \yii\web\Controller
 {
-    public function actionIndex()
-    {
-        return $this->render('view', [
-            'catalog' => Menu::getRoot(),
-        ]);
-    }
+    public $defaultAction = 'view';
 
-    public function actionView($id)
+    public function actionView($id = null)
     {
-        return $this->render('view', [
-            'catalog' => Menu::findById($id),
-        ]);
+        $catalog = isset($id) ? Menu::findById($id) : Menu::getRoot();
+        return $catalog->children(1)->all() ?
+            $this->render('view', [ 'catalog' => $catalog ]) :
+            $this->redirect([ '/product/index', 'category_id' => $catalog->id ]);
     }
 }
---
+++ controllers/ProductController.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace app\controllers;
+
+use Yii;
+use app\models\Good\Good;
+use yii\data\ActiveDataProvider;
+use yii\web\Controller;
+use yii\web\NotFoundHttpException;
+use yii\filters\VerbFilter;
+
+/**
+ * ProductController implements the CRUD actions for Good model.
+ */
+class ProductController extends Controller
+{
+    /**
+     * Lists all Good models.
+     * @return mixed
+     */
+    public function actionIndex()
+    {
+        $dataProvider = new ActiveDataProvider([
+            'query' => Good::find()->where(Yii::$app->request->get()),
+        ]);
+        return $this->render('index', [
+            'dataProvider' => $dataProvider,
+        ]);
+    }
+
+    /**
+     * Displays a single Good model.
+     * @param integer $id
+     * @return mixed
+     */
+    public function actionView($id)
+    {
+        return $this->render('view', [
+            'model' => $this->findModel($id),
+        ]);
+    }
+
+    /**
+     * Finds the Good model based on its primary key value.
+     * If the model is not found, a 404 HTTP exception will be thrown.
+     * @param integer $id
+     * @return Good the loaded model
+     * @throws NotFoundHttpException if the model cannot be found
+     */
+    protected function findModel($id)
+    {
+        if (($model = Good::findOne($id)) !== null) {
+            return $model;
+        } else {
+            throw new NotFoundHttpException('The requested page does not exist.');
+        }
+    }
+}
