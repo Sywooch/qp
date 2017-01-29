3e0e4b7ed2751ab7abc4f4719e17014a485af5b7
pinkslot <pinkslot@github.com>
2017-01-29 13:40:13 +1000

    re-implement components/CatalogRootWidget

--- components/catalog/views/catalog_root.php
+++ components/catalog/views/catalog_root.php
@@ -1,8 +1,25 @@
 <?php
-/** @var $root_ch array of app\models\Good\Menu  */
+/** @var $root app\models\Good\Menu  */
 use yii\helpers\Html;
 
-$depth = 1;
+function traversal($node) {
+    /** @var $node app\models\Good\Menu  */
+    if ($chs = $node->children(1)->all()) {
+        echo Html::beginTag('li' , ['class' => 'has-child']) . "\n";
+        echo Html::a($node->name, ['catalog/view', 'id' => $node->id]);
+        echo Html::beginTag('ul') . "\n";
+        foreach($chs as $ch) {
+            traversal($ch);
+        }
+        echo Html::endTag('ul') . "\n";
+    }
+    else {
+        echo Html::beginTag('li') . "\n";
+        echo Html::a($node->name . "(кол-во товара)", ['catalog/view', 'id' => $node->id]);
+    }
+    echo Html::endTag('li') . "\n";
+}
+
 ?>
 <aside class="sidebar">
     <div class="row">
@@ -10,38 +27,10 @@ $depth = 1;
             <div class="text-subline"></div>
             <ul class="categories-list transform-body">
                 <?php
-                /** @var $item app\models\Good\Menu  */
-                $items = [];
-                for($i = 1; $i < count($root_ch); $i++ ) {
-                    $cur = $root_ch[$i];
-                    $next = $i + 1 > count($root_ch) - 1 ? $cur : $root_ch[$i + 1];
-                    if($cur->depth == $depth) {
-                        echo Html::endTag('li') . "\n";
-                    } else if($cur->depth > $depth) {
-
-                    } else {
-                        for($j = $depth - $cur->depth; $j; $j--) {
-                            echo Html::endTag('ul') . "\n";
-                            echo Html::endTag('li') . "\n";
-                        }
-                    }
-                    if($next->depth > $cur->depth) {
-                        echo Html::beginTag('li', ['class' => 'has-child']) . "\n";
-                        echo Html::a($cur->name, ['catalog/view', 'id' => $cur->id]);
-                        echo Html::beginTag('ul') . "\n";
-
-                    } else {
-                        echo Html::beginTag('li') . "\n";
-                        echo Html::a($cur->name. " (".$cur->depth . ")", ['catalog/view', 'id' => $cur->id]);
-                    }
-
-                    $depth = $cur->depth;
+                foreach($root->children(1)->all() as $ch) {
+                    traversal($ch);
                 }
-                for($i=$depth; $i; $i--)
-                {
-                    echo Html::endTag('li')."\n";
-                    echo Html::endTag('ul')."\n";
-                }
+
                 ?>
             </ul>
         </div>
