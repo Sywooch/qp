1fdb3d331853e63bd0e03cf2aa8207948f236e6a
Sosed <chernov.net@ya.ru>
2017-01-30 15:13:47 +1000

    Adding products to cart using Ajax

--- gulp/js/_cart.js
+++ gulp/js/_cart.js
@@ -1,8 +1,13 @@
 var Cart = (function(){
-    "use strict";
 
     var inputCount = $('.product_count'),
-        compare = $('.btn-compare');
+        compare = $('.btn-compare'),
+        el = $('.shopping');
+
+    //todo: Сделать функцию, которая будет предотвращать множественное отправление ajax запросов
+
+    //Time in milliseconds between Ajax requests
+    var interval = 200;
 
     var __ = {
         init: function() {
@@ -13,6 +18,34 @@ var Cart = (function(){
             inputCount.on('change', function () {
                 self.changeCount($(this));
             });
+
+            compare.on('click', function () {
+                var id = $(this).data('productId'),
+                    count = $(this).attr('data-product-count');
+                self.getData('/catalog/add', {
+                    id: id,
+                    count: count
+                });
+            });
+        },
+        getData: function(url, options){
+            var self = this;
+            $.ajax({
+                url: url,
+                dataType: "html",
+                type: "POST",
+                data: "product-id="+options.id+"&product-count="+options.count,
+                success: function(result){
+                    self.render(result);
+                },
+                error: function () {
+                    console.log('Error');
+                }
+
+            });
+        },
+        render: function (result) {
+            el.html(result);
         },
         changeCount: function (element) {
             var id = element.data('productId'),
