4e1e90034a0e7f6e7b36112c82ec473da665064b
pinkslot <pinkslot@github.com>
2017-02-16 08:19:08 +1000

    apply filters to product list, but don't answer on ajax request

--- controllers/CatalogController.php
+++ controllers/CatalogController.php
@@ -109,6 +109,26 @@ class CatalogController extends \yii\web\Controller
                 ];
             }
         }
+
+        if ($fs = Yii::$app->request->get('f')) {
+            str_replace('Testp', 'p', $fs);
+            $fs = explode(';', $fs);
+            array_pop($fs);
+            foreach($fs as $f) {
+                list($prop, $val) = explode(':', $f);
+                if ($prop == 'p') {
+                    list($min, $max) = explode('-', $val);
+                    $products = array_filter($products, function ($prod) use ($min, $max) {
+                        return 100* (int)$min <= $prod->price && $prod->price <= 100* (int)$max;
+                    });
+                }
+                else {
+                    $products = array_filter($products, function ($prod) use ($prop, $val) {
+                        return isset($prod->properties[$prop]) and $prod->properties[$prop] == $val;
+                    });
+                }
+            }
+        }
         $category = Menu::findOneOr404($cid);
 
         return $this->render('/product/index', [
