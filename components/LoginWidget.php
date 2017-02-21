756e69cd72c3ee309f77878441b60130d6ec56e1
pinkslot <pinkslot@github.com>
2017-02-21 18:58:12 +1000

    replace backend ref in LoginWidget

--- components/LoginWidget.php
+++ components/LoginWidget.php
@@ -16,6 +16,13 @@ class LoginWidget extends Widget
             $items = [
                 ['label' => 'Личный кабинет', 'url' => '/profile/index'],
                 ['label' => 'Избранное', 'url' => '/profile/bookmark'],
+            ];
+
+            if (\Yii::$app->user->can('admin')) {
+                array_push($items, ['label' => 'Зазеркалье', 'url' => '/backend']);
+            }
+
+            $items = array_merge($items, [
                 '<li role="separator" class="divider"></li>',
                 '<li>'
                 . Html::beginForm(['/site/logout'], 'post')
@@ -25,11 +32,7 @@ class LoginWidget extends Widget
                 )
                 . Html::endForm()
                 . '</li>'
-            ];
-
-            if (\Yii::$app->user->can('admin')) {
-                array_unshift($items, ['label' => 'Зазеркалье', 'url' => '/backend']);
-            }
+            ]);
             echo '<li class="dropdown">'
                 . ButtonDropdown::widget([
                     'label' => \Yii::$app->user->identity->email,
