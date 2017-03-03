df7db5ffea7f131564dab919a34b97c6ece11ee6
Sosed <chernov.net@ya.ru>
2017-03-03 20:11:59 +1000

    Add mobile version for filter

--- web/js/app.js
+++ web/js/app.js
@@ -193,7 +193,6 @@ var Cart = (function($){
                     console.log('Error');
                     App.message('Произошла ошибка', false);
                 }
-
             });
         },
 
@@ -277,6 +276,44 @@ var Cart = (function($){
     };
 
     Catalog.init();
+
+})(jQuery);
+(function($){
+
+    "use strict";
+
+    var $handler = $('.qp-collapse-handler');
+
+
+    var Collapse = {
+        init: function() {
+            this.event();
+        },
+        event: function() {
+            var self = this;
+            $handler.on('click', function () {
+                self.shown($(this), $('#' + $(this).data('toggle')));
+            });
+        },
+
+        shown: function (handler, el) {
+            if(window.innerWidth < 769) {
+                if(el.hasClass('shown')){
+                    el.removeClass('shown');
+                    el.addClass('closed');
+                    el.fadeOut(200);
+                    handler.removeClass('active');
+                } else {
+                    el.addClass('shown');
+                    el.removeClass('closed');
+                    el.fadeIn(100);
+                    handler.addClass('active');
+                }
+            }
+        },
+    };
+
+    Collapse.init();
 
 })(jQuery);
 (function($){
