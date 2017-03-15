c678d5f66ae0fb62c0da18ba07fb0a17a1adead2
pinkslot <pinkslot@github.com>
2017-03-15 20:42:10 +1000

    fix #11, fix #14

--- views/profile/_itemEdit.php
+++ views/profile/_itemEdit.php
@@ -7,6 +7,6 @@ use yii\helpers\Html;
     <div class="col-xs-4 name"><?=$item['label']?></div>
     <div class="col-xs-5 value"><?=$item['value']?></div>
     <div class="col-xs-3 edit">
-        <?=Html::a('Изменить', $item['url'])?>
+        <?=Html::a($item['value'] ? 'Изменить' : 'Добавить', $item['url'])?>
     </div>
 </div>
