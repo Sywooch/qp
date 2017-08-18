45eec3acb0c89c99a5cf85c9d44bc7dfbe1c36c9
s.chernov <s.chernov@cy.cardpay.com>
2017-08-18 20:56:53 +1000

    Fix search section in header

--- views/layouts/_search_form.php
+++ views/layouts/_search_form.php
@@ -3,12 +3,10 @@ use yii\helpers\Html;
 use yii\helpers\Url;
 ?>
 
-<div class="col-xs-9 col-md-12">
-    <div class="input-group" id="js-search-wrap">
-        <span class="input-group-addon">
-            <button class="search__btn" data-toggle="modal" data-target="#search-modal"><span class="lnr lnr-magnifier"></span></button>
-        </span>
-        <?= Html::textInput('q', $text, ['class' => 'form-control input-lg', 'id'=> 'js-search-input','placeholder' => 'Поиск...']) ?>
+<div class="input-group" id="js-search-wrap">
+    <span class="input-group-addon">
+        <button class="search__btn" data-toggle="modal" data-target="#search-modal"><span class="lnr lnr-magnifier"></span></button>
+    </span>
+    <?= Html::textInput('q', $text, ['class' => 'form-control input-lg', 'id'=> 'js-search-input','placeholder' => 'Поиск...']) ?>
 
-    </div><!-- /input-group -->
-</div>
+</div><!-- /input-group -->
