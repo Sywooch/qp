8ca219f0c9d1011b340ddea949be83d8924c96b0
pinkslot <pinkslot@github.com>
2017-08-24 01:49:23 +1000

    Change listView to gridView in backend/good

--- modules/backend/views/good/index.php
+++ modules/backend/views/good/index.php
@@ -1,6 +1,7 @@
 <?php
 
-use yii\helpers\Html;
+use yii\grid\GridView;
+use app\components\Html;
 use yii\widgets\ListView;
 
 /* @var $this yii\web\View */
@@ -16,11 +17,77 @@ $this->params['breadcrumbs'][] = $this->title;
     <p>
         <?= Html::a('Добавить товар', ['create'], ['class' => 'btn btn-success']) ?>
     </p>
-    <?= ListView::widget([
+    <?= GridView::widget([
         'dataProvider' => $dataProvider,
-        'itemOptions' => ['class' => 'item'],
-        'itemView' => function ($model, $key, $index, $widget) {
-            return Html::a(Html::encode($model->name), ['view', 'id' => $model->id]);
-        },
-    ]) ?>
+        'columns' => [
+            'id',
+            'name',
+            [
+                'attribute' => 'price',
+                'label' => 'Цена, р.',
+                'value' => function($model) {
+                    /* @var $model app\models\Good\Good */
+                    return Html::unstyled_price($model->getPrice());
+                },
+                'enableSorting'=>TRUE,
+            ],
+            [
+                'attribute' => 'category_id',
+                'label' => 'Категория',
+                'value' => function($model) {
+                /* @var $model app\models\Good\Good */
+                    return $model->category->name;
+                },
+                'enableSorting'=>TRUE,
+            ],
+//            'is_discount',
+            'vendor',
+            'c1id',
+            [
+                'attribute' => 'provider',
+                'label' => 'Поставщик',
+                'value' => function($model) {
+                    /* @var $model app\models\Good\Good */
+                    return $model->getProviderName();
+                },
+                'enableSorting'=>TRUE,
+            ],
+            [
+                'attribute' => 'status',
+                'label' => 'Статус',
+                'value' => function($model) {
+                    /* @var $model app\models\Good\Good */
+                    return $model->getStatusString();
+                },
+                'enableSorting'=>TRUE,
+            ],
+            [
+                'class' => 'yii\grid\ActionColumn',
+                'template' => '{view} {update} {delete}',
+                'buttons' => [
+                    'view' => function ($url,$model) {
+                        return Html::a(
+                            '<i class="fa fa-eye"></i>',
+                            $url);
+                    },
+                    'update' => function ($url,$model) {
+                        return Html::a(
+                            '<i class="fa fa-edit"></i>',
+                            $url);
+                    },
+                    'delete' => function ($url,$model) {
+                        return Html::a(
+                            '<i class="fa fa-trash"></i>',
+                            $url,  [
+                                'data' => [
+                                    'confirm' => 'Вы уверены?',
+                                    'method' => 'post',
+                                ]
+                            ]
+                        );
+                    },
+                ],
+            ],
+        ],
+    ]); ?>
 </div>
