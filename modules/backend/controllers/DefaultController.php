cd9ade7c8b0428761449361aeed72d4beae4150b
pinkslot <pinkslot@github.com>
2017-09-11 02:28:08 +1000

    add report excel export

--- modules/backend/controllers/DefaultController.php
+++ modules/backend/controllers/DefaultController.php
@@ -8,12 +8,18 @@ use app\models\OrderProduct;
 use app\models\User;
 use app\modules\backend\models\UploadProvider;
 use app\modules\backend\models\UploadZipModel;
+use PHPExcel;
+use PHPExcel_Style_NumberFormat;
+use PHPExcel_Writer_Excel2007;
 use Yii;
 use yii\data\ActiveDataProvider;
 use yii\filters\VerbFilter;
 use yii\web\Controller;
 use yii\web\UploadedFile;
 use app\models\Profile\LoginForm;
+use app\components\Html;
+
+require_once dirname(__FILE__) . '/../../../Classes/PHPExcel.php';
 
 /**
  * Default controller for the `admin` module
@@ -124,10 +130,8 @@ class DefaultController extends Controller
         return $this->render('manual');
     }
 
-    public function actionReport()
+    public function reportSelector($start, $end)
     {
-        $start = Yii::$app->request->post('start');
-        $end = Yii::$app->request->post('end');
         $order_products = OrderProduct::find()->joinWith('order')
             ->select(["provider, product_name, product_vendor, product_c1id, old_price,
                 SUM(products_count) AS count_by_c1id,
@@ -142,7 +146,14 @@ class DefaultController extends Controller
             $end_timestamp = strtotime($end) + 24 * 60 * 60;
             $order_products->andWhere(['<=', 'order.created_at', $end_timestamp]);
         }
+        return $order_products;
+    }
 
+    public function actionReport()
+    {
+        $start = Yii::$app->request->get('start');
+        $end = Yii::$app->request->get('end');
+        $order_products = $this->reportSelector($start, $end);
         $dataProvider = new ActiveDataProvider([
             'query' => $order_products,
         ]);
@@ -168,5 +179,58 @@ class DefaultController extends Controller
             'start' => $start,
             'end' => $end,
         ]);
+    }
+
+    public function actionReportExcelExport()
+    {
+        $start = Yii::$app->request->get('start');
+        $end = Yii::$app->request->get('end');
+        $objPHPExcel = new PHPExcel();
+        $objPHPExcel->setActiveSheetIndex(0);
+        $rowCount = 1;
+
+        $objPHPExcel->getActiveSheet()->getStyle('C:C')
+            ->getNumberFormat()
+            ->setFormatCode(PHPExcel_Style_NumberFormat::FORMAT_TEXT);
+        $objPHPExcel->getActiveSheet()->getColumnDimension('C')->setWidth(30);
+        $objPHPExcel->getActiveSheet()->getColumnDimension('D')->setWidth(30);
+        $objPHPExcel->getActiveSheet()->getColumnDimension('E')->setWidth(30);
+        $objPHPExcel->getActiveSheet()->SetCellValue('A'.$rowCount, 'Поставщик');
+        $objPHPExcel->getActiveSheet()->SetCellValue('B'.$rowCount, 'Цена');
+        $objPHPExcel->getActiveSheet()->SetCellValue('C'.$rowCount, 'Название товара');
+        $objPHPExcel->getActiveSheet()->SetCellValue('D'.$rowCount, 'Артикул');
+        $objPHPExcel->getActiveSheet()->SetCellValue('E'.$rowCount, '1С ИД');
+        $objPHPExcel->getActiveSheet()->SetCellValue('F'.$rowCount, 'Заказано товара');
+        $objPHPExcel->getActiveSheet()->SetCellValue('G'.$rowCount, 'Нехватка товара');
+        $rowCount++;
+
+        $selector = $this->reportSelector($start, $end);
+        if ($sort = Yii::$app->request->get('sort')) {
+            if ($sort[0] == '-') {
+                $ordering = SORT_DESC;
+                $sort = substr($sort, 1);
+            }
+            else {
+                $ordering = SORT_ASC;
+            }
+
+            $selector->orderBy([$sort => $ordering]);
+        }
+
+        foreach ($selector->all() as $product) {
+            $objPHPExcel->getActiveSheet()->SetCellValue('A'.$rowCount, $product->provider);
+            $objPHPExcel->getActiveSheet()->SetCellValue('B'.$rowCount, Html::unstyled_price($product->old_price));
+            $objPHPExcel->getActiveSheet()->SetCellValue('C'.$rowCount, $product->product_name);
+            $objPHPExcel->getActiveSheet()->SetCellValue('D'.$rowCount, $product->product_vendor);
+            $objPHPExcel->getActiveSheet()->SetCellValue('E'.$rowCount, $product->product_c1id);
+            $objPHPExcel->getActiveSheet()->SetCellValue('F'.$rowCount, $product->count_by_c1id);
+            $objPHPExcel->getActiveSheet()->SetCellValue('G'.$rowCount, $product->unconfirmed_count_by_c1id);
+            $rowCount++;
+        }
+        $objWriter = new PHPExcel_Writer_Excel2007($objPHPExcel);
+        $file_name = '../temp/report.xlsx';
+        $objWriter->save($file_name);
+        set_time_limit(5*60);
+        Yii::$app->response->sendFile($file_name);
     }
 }
